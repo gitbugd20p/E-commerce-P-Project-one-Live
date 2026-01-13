895f8ed0d899f1e54e99fac5c5c946d5d48fdb78
gitbugd20p <155899250+gitbugd20p@users.noreply.github.com>
2026-01-13 17:36:14 +0600

    fix: fixed cors-origin issue

--- backend/app.js
+++ backend/app.js
@@ -28,10 +28,16 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Other middlewares
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+app.use(
+    cors({
+        origin: "https://sabur-e-commerce-p-project-one-live.vercel.app",
+        credentials: true,
+    })
+);
 
 // Database connection
 const url = process.env.DB_URL;
